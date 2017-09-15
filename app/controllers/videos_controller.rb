e420d3d7e1686bdd023313430f36e07ef509d17a
felipefreitag <ffvargas@gmail.com>
2017-09-15 11:49:29 -0300

    Updates Ruby and Foundation. Starts header and footer layout

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,6 @@
 class VideosController < ApplicationController
-  after_action :verify_authorized, except: %i[index]
-  after_action :verify_policy_scoped, only: %i[index]
+  #after_action :verify_authorized, except: %i[index]
+  #after_action :verify_policy_scoped, only: %i[index]
 
   def index
   end
