7920acc41b78326409788b94b51f9bb79d6b47c1
felipefreitag <ffvargas@gmail.com>
2017-09-21 16:21:04 -0300

    Adds show category styles. Updates Show video.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,5 +2,14 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @videos = @category.videos
+    @series = @category.series
+
+    # @videos.each do |video|
+    #   video_uri = URI("http://vimeo.com/api/v2/video/#{video.vimeo_id}.json")
+    #   json = JSON.parse(Net::HTTP.get(video_uri))
+    #   image_url = json[0]['thumbnail_small']
+    #   video[:image_url] = image_url
+    # end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,28 @@
 section =  Section.create!(
-  name: 'foo section',
+  name: 'Vida em Equilíbrio',
   description: 'some section description',
   image_url: 'http://image-url'
 )
 
 subsection = Subsection.create!(
-  name: "bar subsection",
+  name: 'Mente Sã',
   section: section,
 )
 
 category = Category.create!(
-  name: 'baz category',
+  name: 'Educação Emocional & Comunicação Não Violenta',
   description: 'some category description',
   subsection: subsection,
 )
 
 series = Series.create!(
-  name: 'foo bar series',
+  name: 'Os Portais da Alma',
   description: 'some series description',
   category: category,
 )
 
-video = Video.create!(
-  name: 'foo bar video',
+video1 = Video.create!(
+  name: 'Conheça a Psicologia Quântica',
   description: 'Amig Goswami é uma das maiores autoridades mundiais em Psicologia
     Quântica. Nesta palestra, ele faz um contraponto entre a Psicologia tradicional
     e a Quântica. Ele sua fala ele mostra que o modelo comportamental',
@@ -39,8 +39,80 @@ video = Video.create!(
   category: category,
 )
 
-video_series = Video.create!(
-  name: 'series part 1',
+video2 = Video.create!(
+  name: 'Parto Humanizado',
+  description: 'Amig Goswami é uma das maiores autoridades mundiais em Psicologia
+    Quântica. Nesta palestra, ele faz um contraponto entre a Psicologia tradicional
+    e a Quântica. Ele sua fala ele mostra que o modelo comportamental',
+  information: 'Palestrante: Amit Goswami Tema da Palestra: Psicologia Quântica
+    Integrativa Local: Palácio dos Festivais - Gramado Evento: Conferência Internacional
+    de Saúde Quântica',
+  synopsys: 'Amig Goswami é uma das maiores autoridades mundiais em Psicologia
+    Quântica. Nesta palestra, ele faz um contraponto entre a Psicologia tradicional
+    e a Quântica. Ele sua fala ele mostra que o modelo comportamental',
+  bio: 'Ph.D em Física Quântica, Amit Goswami ficou mundialmente famoso pela
+    sua participação no filme "Quem somos nós?". Com diversos livros publicados
+    e vários artigos científicos, o trabalho de Amit traz',
+  vimeo_id: "163721649",
+  category: category,
+)
+
+video3 = Video.create!(
+  name: 'Pocket Show Banda Curasom',
+  description: 'Amig Goswami é uma das maiores autoridades mundiais em Psicologia
+    Quântica. Nesta palestra, ele faz um contraponto entre a Psicologia tradicional
+    e a Quântica. Ele sua fala ele mostra que o modelo comportamental',
+  information: 'Palestrante: Amit Goswami Tema da Palestra: Psicologia Quântica
+    Integrativa Local: Palácio dos Festivais - Gramado Evento: Conferência Internacional
+    de Saúde Quântica',
+  synopsys: 'Amig Goswami é uma das maiores autoridades mundiais em Psicologia
+    Quântica. Nesta palestra, ele faz um contraponto entre a Psicologia tradicional
+    e a Quântica. Ele sua fala ele mostra que o modelo comportamental',
+  bio: 'Ph.D em Física Quântica, Amit Goswami ficou mundialmente famoso pela
+    sua participação no filme "Quem somos nós?". Com diversos livros publicados
+    e vários artigos científicos, o trabalho de Amit traz',
+  vimeo_id: "163721649",
+  category: category,
+)
+
+video4 = Video.create!(
+  name: 'Encontro com Sri Prem Baba',
+  description: 'Amig Goswami é uma das maiores autoridades mundiais em Psicologia
+    Quântica. Nesta palestra, ele faz um contraponto entre a Psicologia tradicional
+    e a Quântica. Ele sua fala ele mostra que o modelo comportamental',
+  information: 'Palestrante: Amit Goswami Tema da Palestra: Psicologia Quântica
+    Integrativa Local: Palácio dos Festivais - Gramado Evento: Conferência Internacional
+    de Saúde Quântica',
+  synopsys: 'Amig Goswami é uma das maiores autoridades mundiais em Psicologia
+    Quântica. Nesta palestra, ele faz um contraponto entre a Psicologia tradicional
+    e a Quântica. Ele sua fala ele mostra que o modelo comportamental',
+  bio: 'Ph.D em Física Quântica, Amit Goswami ficou mundialmente famoso pela
+    sua participação no filme "Quem somos nós?". Com diversos livros publicados
+    e vários artigos científicos, o trabalho de Amit traz',
+  vimeo_id: "163721649",
+  category: category,
+)
+
+video_series1 = Video.create!(
+  name: 'Os Portais da Alma parte 1',
+  description: 'Amig Goswami é uma das maiores autoridades mundiais em Psicologia
+    Quântica. Nesta palestra, ele faz um contraponto entre a Psicologia tradicional
+    e a Quântica. Ele sua fala ele mostra que o modelo comportamental',
+  information: 'Palestrante: Amit Goswami Tema da Palestra: Psicologia Quântica
+    Integrativa Local: Palácio dos Festivais - Gramado Evento: Conferência Internacional
+    de Saúde Quântica',
+  synopsys: 'Amig Goswami é uma das maiores autoridades mundiais em Psicologia
+    Quântica. Nesta palestra, ele faz um contraponto entre a Psicologia tradicional
+    e a Quântica. Ele sua fala ele mostra que o modelo comportamental',
+  bio: 'Ph.D em Física Quântica, Amit Goswami ficou mundialmente famoso pela
+    sua participação no filme "Quem somos nós?". Com diversos livros publicados
+    e vários artigos científicos, o trabalho de Amit traz',
+  vimeo_id: "163721649",
+  series: series,
+)
+
+video_series2 = Video.create!(
+  name: 'Os Portais da Alma parte 2',
   description: 'Amig Goswami é uma das maiores autoridades mundiais em Psicologia
     Quântica. Nesta palestra, ele faz um contraponto entre a Psicologia tradicional
     e a Quântica. Ele sua fala ele mostra que o modelo comportamental',
