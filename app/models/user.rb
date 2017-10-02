fe6c6da2da7c479e2f07bf49091f05297dafe95a
felipefreitag <ffvargas@gmail.com>
2017-10-02 17:24:57 -0300

    Fixes indentations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable
 
- validates :first_name, presence: true
- validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
- has_many :subscriptions
+  has_many :subscriptions
 
- def subscribed?
-   @subscribed ||= subscriptions.with_state(:active).count > 0
- end
+  def subscribed?
+    @subscribed ||= subscriptions.with_state(:active).count.positive?
+  end
 end
