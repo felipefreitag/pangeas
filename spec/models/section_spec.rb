bc33d70c8f5f3eb62908e13f77a8a7174d6f94ed
felipefreitag <ffvargas@gmail.com>
2017-10-02 17:53:02 -0300

    Changes double quotes for single quotes

--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -3,11 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe Section, type: :model do
-  describe "associations" do
+  describe 'associations' do
     it { expect(subject).to have_many(:subsections) }
   end
 
-  describe "validations" do
+  describe 'validations' do
     it { expect(subject).to validate_presence_of(:name) }
     it { expect(subject).to validate_presence_of(:description) }
     it { expect(subject).to validate_presence_of(:image_url) }
