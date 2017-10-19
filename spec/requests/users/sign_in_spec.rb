7094015f39625f8bbea5270bb4edbc8dae99ebd2
felipefreitag <ffvargas@gmail.com>
2017-10-19 17:18:58 -0200

    Tests redirect after sign-in

---
+++ spec/requests/users/sign_in_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'User sign-in', type: :request do
+  subject { response }
+
+  let!(:user) do
+    User.create!(
+      first_name: 'john',
+      last_name: 'doe',
+      email: 'foo@bar.com',
+      password: '123456'
+    )
+  end
+
+  describe 'after user signs in' do
+    before do
+      post user_session_path \
+        'user[email]': user.email,
+        'user[password]': user.password
+    end
+
+    it 'redirects to sections/index' do
+      expect(subject).to redirect_to(sections_path)
+    end
+  end
+end
