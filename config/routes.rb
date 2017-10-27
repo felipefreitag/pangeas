a4302ae3cd15a8f817106fe9bdc0d4ebe0ae2545
felipefreitag <ffvargas@gmail.com>
2017-10-27 12:16:05 -0200

    Renames routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :categories, only: %i[show]
   resources :sections, only: %i[index show]
   resources :subscriptions, only: %i[new]
-  get '/cstqpangeas/', to: 'high_voltage/pages#show', id: 'landing_cstq'
-  get '/cifsqpangeas/', to: 'high_voltage/pages#show', id: 'landing_cifsq'
-  get '/svl/', to: 'high_voltage/pages#show', id: 'landing_svl'
+  get '/cstqpangeas', to: 'high_voltage/pages#show', id: 'landing_cstq'
+  get '/cifsqpangeas', to: 'high_voltage/pages#show', id: 'landing_cifsq'
+  get '/svl', to: 'high_voltage/pages#show', id: 'landing_svl'
 end
