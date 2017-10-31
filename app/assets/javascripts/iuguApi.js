dbe03c0b4189db6ee15623b0c4dbc3294c63d8e8
felipefreitag <ffvargas@gmail.com>
2017-10-31 17:07:42 -0200

    Adds error messages to payment form

--- app/assets/javascripts/iuguApi.js
+++ app/assets/javascripts/iuguApi.js
@@ -5,11 +5,18 @@ function iuguApi() {
     Iugu.setup();
   }
   $('#payment-form').submit(function(event) {
+    $('#error-list').empty();
     var form = $(this);
     $('button[type=submit]').attr('disabled', true);
     var tokenResponseHandler = function(data) {
       if (data.errors) {
-        console.log("Erro salvando cartão: " + JSON.stringify(data.errors));
+        $('#error-box').removeClass('hidden');
+        errors = JSON.stringify(data.errors);
+        $.each(data.errors, function(index, error) {
+          $('#error-list').append(
+            $('<li></li>').text(index + ': ' + error)
+          )
+        });
         $('button[type=submit]').attr('disabled', false);
       } else {
         $("#token").val(data.id);
