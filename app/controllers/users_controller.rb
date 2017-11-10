a2ac660cc49aff5e9f276a053ab92fed9b9eb637
felipefreitag <ffvargas@gmail.com>
2017-11-10 16:43:24 -0200

    Adds Users#show

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class UsersController < ApplicationController
+  after_action :verify_authorized
+
+  def show
+    @user = User.find(params[:id])
+    authorize @user
+  end
+end
---
+++ spec/requests/users/show_spec.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'GET /users/:id', type: :request do
+  subject { response }
+
+  context 'without logged in user' do
+    let!(:user) do
+      User.create!(
+        first_name: 'jane',
+        last_name: 'doe',
+        email: 'bar@baz.com',
+        password: '123456'
+      )
+    end
+
+    before do
+      get "/users/#{user[:id]}"
+    end
+
+    it { is_expected.to redirect_to(new_user_session_path) }
+  end
+
+  context 'with logged in user' do
+    let!(:user) do
+      User.create!(
+        first_name: 'jane',
+        last_name: 'doe',
+        email: 'bar@baz.com',
+        password: '123456'
+      )
+    end
+
+    context 'that is the owner' do
+      before do
+        sign_in user
+        get "/users/#{user[:id]}"
+      end
+
+      it { is_expected.to have_http_status(:ok) }
+    end
+
+    context 'that is not the owner' do
+      let(:user2) do
+        User.create!(
+          first_name: 'john',
+          last_name: 'doe',
+          email: 'foo@baz.com',
+          password: '123456'
+        )
+      end
+
+      before do
+        sign_in user2
+        get "/users/#{user[:id]}"
+      end
+
+      it { is_expected.to redirect_to(new_subscription_path) }
+    end
+
+    context 'that is admin' do
+      let(:user2) do
+        User.create!(
+          first_name: 'john',
+          last_name: 'doe',
+          email: 'foo@baz.com',
+          password: '123456',
+          admin: true
+        )
+      end
+
+      before do
+        sign_in user2
+        get "/users/#{user[:id]}"
+      end
+
+      it { is_expected.to have_http_status(:ok) }
+    end
+  end
+end
